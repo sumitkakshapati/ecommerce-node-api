ef1eb5b605bbd0ff0f683da27e09f09003b8aa7f
sumitkakshapati <me.summet@gmail.com>
2023-05-04 18:16:04 +0545

    cors issue fixes

--- index.js
+++ index.js
@@ -20,6 +20,13 @@ Sentry.init({ dsn: process.env.SENTRY_DSN });
 const PORT = process.env.PORT || 3000;
 const app = express();
 
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,PUT,PATCH,POST,DELETE");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 // This request handler must be the first middleware on the app
 app.use(Sentry.Handlers.requestHandler());
 
@@ -35,12 +42,6 @@ mongoose
 app.use(express.json());
 app.use(morgan("tiny"));
 app.use(helmet());
-app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET,PUT,PATCH,POST,DELETE");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
 
 app.use("/api/auth", authRoute);
 app.use("/api", userRoute);
