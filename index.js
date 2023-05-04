e2948e371452e7e946069c726cddc9e6909d2c15
sumitkakshapati <me.summet@gmail.com>
2023-05-04 18:30:51 +0545

    cors issue fixes

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ import * as errorHandler from './middleware/errorHandler.js';
 import * as Sentry from '@sentry/node';
 import morgan from 'morgan';
 import helmet from 'helmet';
+import cors from 'cors';
 
 dotenv.config();
 
@@ -20,12 +21,9 @@ Sentry.init({ dsn: process.env.SENTRY_DSN });
 const PORT = process.env.PORT || 3000;
 const app = express();
 
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET,PUT,PATCH,POST,DELETE");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+app.use(cors({
+  origin: '*'
+}));
 
 // This request handler must be the first middleware on the app
 app.use(Sentry.Handlers.requestHandler());
@@ -42,6 +40,7 @@ mongoose
 app.use(express.json());
 app.use(morgan("tiny"));
 app.use(helmet());
+
 
 app.use("/api/auth", authRoute);
 app.use("/api", userRoute);
